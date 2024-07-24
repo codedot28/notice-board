6cb88f295555a9f6b1767e881a48db5ac8d91c11
jangkyum <codedot28@gmail.com>
2024-07-24 16:45:20 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+  ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 100,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto', // 한줄 추가
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
+  },
+};
